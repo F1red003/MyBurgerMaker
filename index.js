ff0b101313f97a42951388e9e18a0f9f15580ccc
f1red <idaissafarid03@gmail.com>
2024-12-22 17:28:44 +0100

    trying to add the topbun the restart btn the price erea the   restart btn and finish btn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var z = 0;
 var t = 0;
-var listIngredient=[];
+var listIngredient = [];
+var prix=0;
+const price= document.getElementById("price");
+price.textContent = prix.toFixed(2) + " DH";
+
 
 const addIngredient = (ingredient) => {
   z = z + 10;
@@ -13,52 +17,71 @@ const addIngredient = (ingredient) => {
   switch (true) {
     case ingredientElement.classList.contains("tomato"):
       t = t + 25;
-      listIngredient+="tamato";
+      prix+=1.5;
+      listIngredient += "tamato";
+      price.textContent = prix.toFixed(2) + " DH";
       break;
-    
+
     case ingredientElement.classList.contains("beef"):
       t = t + 25;
-      listIngredient+="tamato";
+      prix+=20;
+      listIngredient += "tamato";
+      price.textContent = prix.toFixed(2) + " DH";
       break;
 
     case ingredientElement.classList.contains("edam"):
       t = t + 25;
-      listIngredient+="edam";
+      prix+=3;
+      listIngredient += "edam";
+      price.textContent = prix.toFixed(2) + " DH";
       break;
 
     case ingredientElement.classList.contains("lettuce"):
       t = t + 25;
-      listIngredient+="lettuce";
+      prix+=1.5;
+      listIngredient += "lettuce";
+      price.textContent = prix.toFixed(2) + " DH";
       break;
     case ingredientElement.classList.contains("mayonnaise"):
       t = t + 19;
-      listIngredient+="mayonnaise";
+      prix+=1;
+      listIngredient += "mayonnaise";
+      price.textContent = prix.toFixed(2) + " DH";
       break;
     case ingredientElement.classList.contains("Ketchup"):
       t = t + 25;
-      listIngredient+="ketchup";
+      prix+=1;
+      listIngredient += "ketchup";
+      price.textContent = prix.toFixed(2) + " DH";
       break;
 
     case ingredientElement.classList.contains("onion"):
-        if(listIngredient!=[]){
-            t=t+3;
-        }else{
-            t=t+30;
-        }
+      if (listIngredient != []) {
+        t = t + 3;
+      } else {
+        t = t + 30;
+      }
       
-      listIngredient+="onion";
+      prix+=1.5;
+      listIngredient += "onion";
+      price.textContent = prix.toFixed(2) + " DH";
       break;
     case ingredientElement.classList.contains("pickle"):
-        if(listIngredient!=[]){
-            t=t+3;
-        }else{
-            t=t+30;
-        }
-      listIngredient+="cucumber";
-      console.log(listIngredient);
+      if (listIngredient != []) {
+        t = t + 3;
+      } else {
+        t = t + 30;
+      }
+      prix+=1.5;
+      listIngredient += "cucumber";
+      price.textContent = prix.toFixed(2) + " DH";
       break;
-    case ingredientElement.classList.contains("topbread"):
-      t = t + 17;
+    
+    case ingredientElement.classList.contains("top-bun"):
+      t = t +70;
+      listIngredient += "top-bun";
+      prix+=2;
+      price.textContent = prix.toFixed(2) + " DH";
       break;
   }
 
@@ -70,3 +93,5 @@ const addIngredient = (ingredient) => {
 
   Burger.insertBefore(ingredientElement, Burger.firstChild);
 };
+
+
